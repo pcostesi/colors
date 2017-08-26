bd6e6785749bc2142f1cd1bce0f59b8feaae1a6b
Pablo Alejandro Costesich <rhfixer@gmail.com>
2017-08-26 19:22:31 -0300

    Implement support for Typescript components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Colors of SABF`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-typescript`
+  ],
 }
