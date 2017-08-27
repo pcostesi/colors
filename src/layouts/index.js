9403045e0c7732f9baac9e3d570fd6f2e9c1010d
Pablo Alejandro Costesich <rhfixer@gmail.com>
2017-08-27 00:33:19 -0300

    Remove old styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,52 +5,19 @@ import Helmet from 'react-helmet'
 
 import './index.css'
 
-const Header = () =>
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+const Header = () => <div></div>
 
 const TemplateWrapper = ({ children }) =>
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Colors of SABF"
       meta={[
-        { name: 'description', content: 'Sample' },
+        { name: 'description', content: 'Everyday stories from awesome people' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
