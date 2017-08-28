ac741ac20cbabef5caa44582927573d2c6922d4b
Pablo Alejandro Costesich <rhfixer@gmail.com>
2017-08-28 13:29:37 -0300

    Fix missing prop types using different import

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import * as React from "react";
+import { PropTypes } from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './index.scss';
 
-const Header = () => <div></div>
+const Header = () => <div>Nothing in the Header</div>
 
 const TemplateWrapper = ({ children }) =>
   <div>
